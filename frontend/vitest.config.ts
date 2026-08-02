997c5645670b43d9b77895a196ed0817f9360afa
GravityMg <GravityWavesManagement@gmail.com>
2026-08-02 19:44:26 +0330

    Update vitest.config.ts

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/tests/setup.tsx'],
+    environment: 'node',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    exclude: ['node_modules/', '.next/', 'dist/'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    clearMocks: true,
+    setupFiles: ['./src/tests/setup.ts'],
   },
 })
